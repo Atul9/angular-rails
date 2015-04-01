995158f476ce5761d50712036b16189b38bd50ee
Atul Bhosale <atul@joshsoftware.com>
2015-04-01 16:27:19 +0530

    Added users controller with views

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+  end
+
+  def index
+    @user = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to users_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_path
+  end
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
+end
